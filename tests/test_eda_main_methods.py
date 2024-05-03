f11d2f6fb0aa6a57dc25c6aed2804acd8fb9837a
Juan Quintana <juanq79@pm.me>
2024-05-03 21:32:26 +0200

    Add tests for all methods

--- tests/test_eda_main_methods.py
+++ tests/test_eda_main_methods.py
@@ -1,9 +1,27 @@
 from data import Samples
 from DAT import EDA
+import pandas as pd
 
 ## load iris dataset
 sp = Samples()
 data = sp.load_dataset_iris()
+
+## test eda empty df validation
+def test_empty_df_validation():
+    # initialize class
+    eda = EDA()
+    # test if df is empty or not
+    assert eda._validate_if_df_empty(pd.DataFrame()) is True
+    assert eda._validate_if_df_empty(data) is False
+    # test in case of input is not a df
+    try:
+        _ = eda._validate_if_df_empty(999)
+        is_done = True
+    except:
+        is_done = False
+    # test
+    assert is_done is False
+
 
 ## test eda info method is working
 def test_eda_info():
@@ -82,6 +100,48 @@ def test_eda_summary():
     # describe info
     try:
         _ = eda.summary(data)
+        is_done = True
+    except:
+        is_done = False
+    # test
+    assert is_done
+
+
+## test eda temporal method is working
+def test_eda_temporal():
+    # initialize class
+    eda = EDA()
+    # describe info
+    try:
+        _ = eda.temporal(data)
+        is_done = True
+    except:
+        is_done = False
+    # test
+    assert is_done
+
+
+## test eda bivariate method is working
+def test_eda_bivariate():
+    # initialize class
+    eda = EDA()
+    # describe info
+    try:
+        _ = eda.bivariate(data)
+        is_done = True
+    except:
+        is_done = False
+    # test
+    assert is_done
+
+
+## test eda duplicates method is working
+def test_eda_duplicates():
+    # initialize class
+    eda = EDA()
+    # describe info
+    try:
+        _ = eda.duplicates(data)
         is_done = True
     except:
         is_done = False
